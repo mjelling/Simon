610fe03928d0da510c4f632b96a391103a257df1
Matthew Jelling <Jelling@MacBook-Pro.local>
2016-03-28 09:58:12 -0400

    init commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+//the first step is to create the arrays that will have the possible sequences for each level
+//create a variable that uses Math.random() and math,floor() to create an integer to select from the nested arrays.
+//create a function that animates the four boxes, that can be used again and again through out the game, this function will need a for loop that will respond to the different length of arrays
+//prompt user to click on the elements in the right order based on what has just been given
+//click animates elements the same way as before
+//log click events, push them into an array, check the new array against the old array
+//if the arrays match, change color background, and add the level before to the UL list
+//if you lose alert the player they have lost, ask if they want to play again, if no, return to page with one levels on it, if yes reload original page.
