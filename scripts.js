1e5155964553d2af4fa1b3a2684612b005883d61
Matthew Jelling <Jelling@MacBook-Pro.local>
2016-03-28 11:56:00 -0400

    adds init js functions

--- scripts.js
+++ scripts.js
@@ -1,3 +1,49 @@
+var counter=1;
+var sequence=[];
+var clicksArray=[];
+var userInput=[];
+function buildSequence() {
+  for(var i=0; i<counter; i++){
+    var x=Math.floor(Math.random()*4);
+    sequence.push(x);
+  console.log(sequence);
+  };
+};
+
+function logClicks(){
+  $('.box').click(function(e) {
+    clicksArray.push(e.target.id); // then e.srcElement.className has the class
+  });
+};
+
+function arrayConverter(){
+  for(var i=0; i<clicksArray.length; i++){
+    if(clicksArray[i]==="green"){
+      userInput.push(0);
+    }
+    else if(clicksArray[i]==="red"){
+      userInput.push(1);
+    }
+    else if(clicksArray[i]==="blue"){
+      userInput.push(2);
+    }
+    else if(clicksArray[i]==="yellow"){
+      userInput.push(3);
+    }
+  };
+};
+
+
+
+$( document ).ready(function() {
+    logClicks();
+});
+
+
+
+
+
+
 //the first step is to create the arrays that will have the possible sequences for each level
 //create a variable that uses Math.random() and math,floor() to create an integer to select from the nested arrays.
 //create a function that animates the four boxes, that can be used again and again through out the game, this function will need a for loop that will respond to the different length of arrays
